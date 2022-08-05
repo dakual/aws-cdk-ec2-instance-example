3859af90a58880f436453ea8965683dfa7bb5b9f
daghan <daghan.altunsoy@gmail.com>
2022-08-05 14:22:54 +0300

    first commit

--- lib/simple-ec2-stack.ts
+++ lib/simple-ec2-stack.ts
@@ -1,16 +1,85 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+import { readFileSync } from 'fs';
 
-export class SimpleEc2Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class SimpleEc2Stack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 👇 create VPC in which we'll launch the Instance
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration: [
+        { name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Get the default VPC.
+    // const vpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true })
+
+    // create Security Group for the Instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'simple-instance-1-sg',
+    });
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    // create a Role for the EC2 Instance
+    const webserverRole = new iam.Role(this, 'webserver-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
+      ],
+    });
+
+    // create the EC2 Instance
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      // vpc: defaultVpc,
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      role: webserverRole,
+      securityGroup: webserverSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'mykey',
+    });
+
+
+    // load contents of script
+    const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
+    // add the User Data script to the Instance
+    ec2Instance.addUserData(userDataScript);
+
+    // cdk lets us output prperties of the resources we create after they are created
+    new cdk.CfnOutput(this, 'simple-instance-1-output', {
+      value: ec2Instance.instancePublicIp
+    })
   }
 }
